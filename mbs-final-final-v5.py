8a815de2c51000f15d57ffeed10858401d137cce
Noah Fields <noah@DN0a2723ec.SUNet>
2023-05-04 16:21:02 -0700

    Created Main

--- mbs-final-final-v5.py
+++ mbs-final-final-v5.py
@@ -28,14 +28,20 @@ def mandelbrot(h, w, x=-0.5, y=0, z=1, mi=100):
         m[np.abs(z) > 2] = False
     return t
 
-try:
- a1 = sys.argv[1]
- a2 = sys.argv[2]
-except* IndexError:
- print('you need args')
- sys.exit(1)
+def main():
+    try:
+    a1 = sys.argv[1]
+    a2 = sys.argv[2]
+    except* IndexError:
+    print('you need args')
+    sys.exit(1)
 
-plt.imsave('out.jpg', mandelbrot(int(sys.argv[1]), int(sys.argv[2])), cmap='magma')
-m = mahotas.imread('out.jpg')
-pylab.imshow(m)
-pylab.show()
+    plt.imsave('out.jpg', mandelbrot(int(sys.argv[1]), int(sys.argv[2])), cmap='magma')
+    m = mahotas.imread('out.jpg')
+    pylab.imshow(m)
+    pylab.show()
+
+
+if __name__ == "__main__":
+    main()
+
